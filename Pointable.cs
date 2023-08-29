d3a63054474b3f6d1f4913b78c2b411b2085980c
SAGE-Rebirth <abhinavs1711@gmail.com>
2023-08-29 19:04:41 +0530

    Pointable uploaded

---
+++ Pointable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pointable : MonoBehaviour
+{
+    public UnityEvent OnEnter;
+    public UnityEvent OnExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Pointer.Instance.CurrentPointable = this;
+        OnEnter.Invoke();
+
+    }
+}
